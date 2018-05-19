ca29ffec8c6f2a24bbcaa35617d1f9be28020a18
Joey Diperi <joey.diperi@C02QQAHKG8WP.local>
2018-05-19 09:35:50 -0400

    added main carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MainPageCarousel from './components/MainPageCarousel';
 
 class App extends Component {
   state={
@@ -37,6 +38,16 @@ class App extends Component {
             active={active}
             openNav={this.openNav}
             closeNav={this.closeNav}
+          />
+          < MainPageCarousel 
+            images = {['untitled-5','untitled-6']}
+            settings = {{
+                className: "center",
+                centerMode: true,
+                infinite: true,
+                slidesToShow: 1,
+                speed: 500,
+            }}
           />
 
             test
---
+++ src/components/MainPageCarousel.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Slider from "react-slick";
+
+// function SamplePrevArrow(props) {
+//     const { className, style, onClick } = props;
+//     let left = require('../images/left-arrow.png'),
+//     leftUrl = `url(${left}`
+//     return (
+//       <div
+//         className={className}
+//         style={{ ...style, display: "block", background: leftUrl, width:'44px', height:"76px"}}
+//         onClick={onClick}
+//       />
+//     );
+//   }
+
+// function SampleNextArrow(props) {
+//     const { className, style, onClick } = props;
+//     let right = require('../images/right-arrow.png'),
+//         rightUrl = `url(${right}`
+
+//     return (
+
+//       <div
+//         className={className}
+//         style={{ ...style, display: "block", background: rightUrl, width:'44px', height:"76px"}}
+//         onClick={onClick}
+//       />
+//     );
+//   }
+
+  
+class MainPageCarousel extends React.Component {
+  constructor(){
+    super();
+
+
+    // this.props.openShopModal(this.state.image1)
+  }
+  render() {
+    let { images, imagesDisplayed, settings} = this.props
+    return (
+      <div className='container main_carousel'>
+      <Slider {...settings}>
+        {images.map((slide, i) => {
+          return (
+            <div key={i}>
+             <img  src={require(`../images/highlights/${slide}.jpg`)} />
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+    );
+}
+}
+
+export default MainPageCarousel;
